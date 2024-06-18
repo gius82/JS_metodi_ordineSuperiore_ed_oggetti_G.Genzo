786e1e20b2f9771e7579c324921c9b3665ec65ef
Giulio Genzo <giuliogenzo@gmail.com>
2024-06-18 16:11:58 +0200

    esercizio1 console segnala errore

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
-Esercizo 1
-let  
+Esercizo 
+
+ let array = [1, 5 ,7 ,12]; Output = 25; 
+
+ let numeri =[2,4,6,8]
+
+ let sommanumeri= numeri.reduce((acc, el)=>acc+el)
+
+ console.log(sommaNumeri)
